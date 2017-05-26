b013679cb7aed7010d2af0ce3432a1f9f174bcd7
Matthew Owen-Jones <matthew_oj@outlook.com>
2017-05-27 08:56:49 +1000

    Post Controller

---
+++ Blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index( $id = null ) {
+        return ($id !== null) ? $this->getCache( 'posts' ) : $this->getCache( $id );
+    }
+
+    public function create( Request $request ) {
+        Post::insert([
+            'user_id' => $request->user()->id,
+            'active' => $request->input('active'),
+            'title' => $request->input ('title'),
+            'body' => $request->input('body'),
+            'published_at' => Carbon::now()
+        ]);
+
+        $this->resetCache();
+    }
+
+    public function update( Request $request ) {
+        $post = Post::findOrFail( $request->input('id') );
+
+        $post->fill([
+            'active' => $request->input('active'),
+            'title' => $request->input('title'),
+            'body' => $request->input('body'),
+        ]);
+
+        $this->resetCache( $post->id );
+    }
+
+    private function getCache( $ref ) {
+        switch( $ref ) {
+            case 'posts':
+                return Cache::remember( $ref, $this->timeout, function() {
+                    return Post::all();
+                });
+            default:
+                return Cache::remember( 'post_' . $ref, $this->timeout, function() use ($ref) {
+                    return Post::find($ref);
+                });
+        }
+    }
+
+    private function resetCache( $single = null ) {
+        if( $single ) Cache::forget( 'post_' . $single );
+        
+        Cache::forget( 'posts' );
+    }
+}
